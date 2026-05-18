7ea95a434db8a4ae323a4e6905c9d4c352991148
iMi <pufferhaus@fugu.estate>
2026-05-18 14:01:20 -0700

    build: Xcode project scaffold via XcodeGen

---
+++ Engram/App/EngramApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct EngramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Engram/UI/ContentView.swift
@@ -0,0 +1,9 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("ENGRAM")
+            .font(.largeTitle)
+            .fontWeight(.black)
+    }
+}
